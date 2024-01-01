b72f1547f0d23f1b56ca7717cc6dc3b9e9841167
Nazmul <mdnazmulhasan0879@gmail.com>
2024-01-01 12:17:50 +0600

    some fix problem

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function myFunction(){
 
     var isvalid = true;
     var email = document.forms["myForm"]["email"].value;
-     if(email.length < 1 ){
+     if(email.length < 5 ){
         error.innerHTML = "Place input your currect email";
         input_eml.style.borderColor = "red";
         input_eml.style.backgroundColor = "rgba(255, 0, 0, 0.089)";
@@ -16,16 +16,21 @@ function myFunction(){
 
 function myFunction2(){
     var error2 = document.querySelector(".error2");
-    var input_eml2 = document.querySelector(".email-offer");
+    var input_eml2 = document.querySelector(".email-offer2");
 
-    var isvalid2 = true;
-    var email = document.forms["myForm2"]["email2"].value;
-     if(email.length < 1 ){
-        error2.innerHTML = "Place input your currect email";
-        input_eml2.style.borderColor = "red";
-        input_eml.style.backgroundColor = "rgba(255, 0, 0, 0.089)";
-        isvalid2 = false;
+   var isRight = true;
+    var email2 = document.forms["myForm2"]["email2"].value;
+     if(email2.length < 5 ){
+       error2.innerHTML = "something wrong";
+       input_eml2.style.borderColor = "red";
+       input_eml2.style.backgroundColor = "rgba(255, 0, 0, 0.089)";
+       isRight = false;
      };
 
-     return isvalid2;
+     return isRight;
 };
+
+// error2.innerHTML = "Place input your currect email";
+// input_eml.style.backgroundColor = "rgba(255, 0, 0, 0.089)";
+// input_eml2.style.borderColor = "red";
+// isRight = false;
